9cedfac5a96c13a833bce945638e8a20e528c827
Alexis Gaudin <alexis@MacBook-Air-de-Alexis.local>
2022-05-26 12:14:18 +0200

    working create_regex_pattern, but [a-z] instead of [^yzub]

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ def check_input(inputs):
     last_input = inputs[-1]
     last_input_length = len(last_input)
 
-    if (not last_input[0].isalpha()):
-        raise Exception("First character should be a letter")
-
     if (last_input_length < 4 or last_input_length > 12):
         raise Exception("This program only handles inputs from 4 to 12 letters")
 
@@ -23,37 +20,66 @@ def create_regex_pattern(inputs, outputs):
     wrong_letters = []
     misplaced_letters = []
 
-    for i in range(len(output)):
-        if output[i] == 'g':
-            pattern.append(input[i])
-        elif output[i] == 'm':
-            pattern.append('[^' + input[i] + ']')
-            misplaced_letters.append(input[i])
-        elif output[i] == 'w':
-            wrong_letters.append(input[i])
+    nth_characters = list(zip(*outputs))
+
+    for i, nth_character in enumerate(nth_characters):
+        for j, character in enumerate(nth_character):
+            letter = inputs[j][i]
+
+            if character == 'g':
+                pattern += letter
+            elif character == 'w':
+                wrong_letters.append(letter)
+            elif character == 'm':
+                misplaced_letters.append(letter)
+            
+        if 'g' not in nth_character:
+            pattern += '[a-z]'
+    
+    pattern +=  ')'
 
     for misplaced_letter in misplaced_letters:
-        pattern.append('(?=.*' + misplaced_letter + '.*)')
+        pattern += '(?=.*' + misplaced_letter + '.*)'
+    
+
+    print('wrong letters: ', wrong_letters)
+    print('misplaced letters: ', misplaced_letters)
+
+    # for i in range(len(output)):
+    #     if output[i] == 'g':
+    #         pattern.append(input[i])
+    #     elif output[i] == 'm':
+    #         pattern.append('[^' + input[i] + ']')
+    #         misplaced_letters.append(input[i])
+    #     elif output[i] == 'w':
+    #         wrong_letters.append(input[i])
+
+    # for misplaced_letter in misplaced_letters:
+    #     pattern.append('(?=.*' + misplaced_letter + '.*)')
 
     return pattern
 
 def main():
-    data = {'words':['slope', 'stove', 'store', 'stone', 'stena']}
-    words_df = pd.DataFrame(data)
+    # data = {'words':['slope', 'stove', 'store', 'stone', 'stena']}
+    # words_df = pd.DataFrame(data)
 
-    regex_pattern = '(?=s[^wy][a-z][a-z]e)(?=.*r.*)(?=.*t.*)'
+    # regex_pattern = '(?=s[^wy][a-z][a-z]e)(?=.*r.*)(?=.*t.*)'
+
+    # input_ = input("Fill in the input, replace missing letters by *, e.g. t*rtu*: ")
+    input_ = 'voiture'
+    inputs = [input_]
+    
+    check_input(inputs)
+    words_df = get_words(f"data/mots_{len(input_)}.txt")
+    print(words_df)
+    
+    # output = input("Fill in the result code (good g, misplaced m, wrong g), e.g. gwgggm: ")
+    output = 'ggwgwwg'
+    outputs = [output]
+
+    regex_pattern = create_regex_pattern(inputs, outputs)
+    print(regex_pattern)
 
     words_left_df = get_words_matching_pattern(words_df, regex_pattern)
     print(words_left_df)
-
-    # input_ = input("Fill in the input, replace missing letters by *, e.g. t*rtu*: ")
-    # inputs = [input_]
-    #
-    # check_input(inputs)
-    # words_df = get_words(f"data/mots_{len(input_)}.txt")
-    # print(words_df)
-    #
-    # output = input("Fill in the result code (good g, misplaced m, wrong g), e.g. gwgggm: ")
-    # outputs = [output]
-
 main()
