afc6a016da7cb8b3a511d2d3d3a181faad269de8
Alexis Gaudin <alexis@MacBook-Air-de-Alexis.local>
2022-05-27 06:51:04 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
 from black import out
 import pandas as pd
 
-def check_inputs(input_, output):
-    if len(input_) != len(output):
-        raise ValueError("Inputs and Outputs should be the same length") 
+def check_inputs(word, colors):
+    if len(word) != len(colors):
+        raise ValueError("Words and Outputs should be the same length") 
     
-    if (len(input_) < 4 or len(input_) > 12):
-        raise ValueError("This program only handles inputs from 4 to 12 letters")
+    if (len(word) < 4 or len(word) > 12):
+        raise ValueError("This program only handles words from 4 to 12 letters")
             
 def get_dictionary(filename):
     dictionary_df = pd.read_csv(filename, sep=" ", header=None)
     dictionary_df.columns = ["words"]
     return dictionary_df
 
-def get_matching_words(dictionary_df, pattern):
-    return dictionary_df.loc[dictionary_df["words"].str.match(pattern, case=False)]
-
-def get_history(inputs, outputs):
-    outputs_transposed = list(zip(*outputs))
+def create_history(all_words, all_colors):
+    colors_transposed = list(zip(*all_colors))
 
     history = []
 
-    for i, nth_character in enumerate(outputs_transposed):
-        output_summary = {"good": '', "wrong": '', "misplaced": ''}
+    for i, colors_nth_letter in enumerate(colors_transposed):
+        history_nth_letter = {"reds": '', "blues": '', "yellows": ''}
 
-        for j, character in enumerate(nth_character):
-            letter = inputs[j][i]
+        for j, color in enumerate(colors_nth_letter):
+            letter = all_words[j][i]
 
-            if character == 'g' and letter not in output_summary["good"]:
-                output_summary["good"] = letter
-            elif character == 'w' and letter not in output_summary["wrong"]:
-                output_summary["wrong"] += letter
-            elif character == 'm' and letter not in output_summary["misplaced"]:
-                output_summary["misplaced"] += letter
+            if color == 'r' and letter not in history_nth_letter["reds"]:
+                history_nth_letter["reds"] = letter
+            elif color == 'b' and letter not in history_nth_letter["blues"]:
+                history_nth_letter["blues"] += letter
+            elif color == 'y' and letter not in history_nth_letter["yellows"]:
+                history_nth_letter["yellows"] += letter
 
-        history.append(output_summary)
+        history.append(history_nth_letter)
     
     return history
 
@@ -44,42 +41,45 @@ def get_letters_in_category(history, category):
         letters += row[category]
     return letters
 
-def create_regex_pattern(history, wrongs, misplaced):
+def create_regex_pattern(history, wrongs, yellows):
     pattern = '(?='
 
     for row in history:
-        if row["good"]:
-            pattern += row["good"][0]
+        if row["reds"]:
+            pattern += row["reds"][0]
         else:
-            pattern += '[^' + wrongs + "".join(row["misplaced"]) + ']'
+            pattern += '[^' + wrongs + "".join(row["yellows"]) + ']'
     
     pattern += ')'
 
-    for misplaced_letter in misplaced:
-        pattern += '(?=.*' + misplaced_letter + '.*)'
+    for yellow in yellows:
+        pattern += '(?=.*' + yellow + '.*)'
 
     return pattern
 
+def get_matching_words(dictionary_df, pattern):
+    return dictionary_df.loc[dictionary_df["words"].str.match(pattern, case=False)]
+
 def main():
     # New request
-    input_ = input("Fill in the input, replace missing letters by *, e.g. t*rtu*: ")
-    output = input("Fill in the result code (good g, misplaced m, wrong g), e.g. gwgggm: ")
-    check_inputs(input_, output)
+    word = input("Fill in the input, replace missing letters by *, e.g. t*rtu*: ")
+    colors = input("Fill in the result code (reds g, yellows m, blues g), e.g. gwgggm: ")
+    check_inputs(word, colors)
 
     # Store results for each iteration
-    inputs.append(input_)
-    outputs.append(output)
+    all_words.append(word)
+    all_colors.append(colors)
 
     # Loading appropriate dictionary
-    dictionary_df = get_dictionary(f"data/mots_{len(input_)}.txt")
+    dictionary_df = get_dictionary(f"data/mots_{len(word)}.txt")
     
     # Compile results into a list of dictionaries (history)
-    history = get_history(inputs, outputs)
-    wrongs = get_letters_in_category(history, "wrong")
-    misplaced = get_letters_in_category(history, "misplaced")
+    history = create_history(all_words, all_colors)
+    wrongs = get_letters_in_category(history, "blues")
+    yellows = get_letters_in_category(history, "yellows")
 
     # Search for matching words in dictionary
-    regex_pattern = create_regex_pattern(history, wrongs, misplaced)
+    regex_pattern = create_regex_pattern(history, wrongs, yellows)
     matching_words = get_matching_words(dictionary_df, regex_pattern)
     print(f"{len(matching_words)} words left:")
     print(matching_words.to_string())
@@ -88,7 +88,7 @@ def main():
     main()
 
 # Initialization of request results
-inputs = []
-outputs = []
+all_words = []
+all_colors = []
 matching_words = pd.DataFrame()
 main()
